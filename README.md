8e48955e8acfebf2a9e830e65914ba6c4accc336
Jody Heavener <jody@pixelunion.net>
2015-05-03 12:13:03 -0600

    Commit dat shit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Socket
+
+Really simple module for managing multiple Socket.io connections.
+
+Cool.
+
+### Class `Socket`
+
+**Config:** Requires HTTP server `Object` as argument so Socket.io can listen to it
+
+**Methods:**
+- `on(eventName{String}, callback{Function})` and `trigger(eventName{String}, data{Array})` to listen to and trigger events
+- `find(key{String}, value{Mixed})` to retrieve a Connection by alias
+- `remove(socket{Object})` to remove a specific Connection
+
+**Events**
+- `message(data{Object}, socket{Object})` a socket Connection recieves a message
+- `disconnect(aliases{Object})` a socket Connection is disconnected; passes the Connection's aliases for identification purposes
+- `connection(connection{Object})` a new socket Connection is created.
+
+### Class `Socket.Connection`
+
+**Config:** Requires Socket.io Connection `Object`
+
+**Methods:**
+- `on(eventName{String}, callback{Function})` and `trigger(eventName{String}, data{Array})` to listen to and trigger events
+- `addAlias(key{String}, value{Mixed})` to add a new identifying property to the current Connection
+- `hasAlias(key{String}, value{Mixed})` returns Boolean for if the current Connection has the specified alias
+- `send(data{Object})` send a message from the current Connection
+- `isConnected()` returns Boolean for if the current Connection is still connected
---
+++ socket.js
@@ -0,0 +1,105 @@
+let Socketio = require("socket.io");
+
+let Socket = class {
+
+  constructor(server) {
+    if (!server) return console.error("Socket instance requires `server` argument");
+
+    this.events = {};
+    this.connections = [];
+
+    this.io = Socketio.listen(server);
+
+    this.io.sockets.on("connection", (connection) => {
+      let socket = new Socket.Connection(connection);
+
+      this.connections.push(socket);
+
+      connection.on("message", (data) => {
+        this.trigger("message", [ data, socket ]);
+      });
+
+      connection.on("disconnect", () => {
+        this.trigger("disconnect", [ socket.aliases ]);
+        this.remove(socket);
+      });
+
+      this.trigger("connection", [ socket ]);
+    });
+
+    return this;
+  };
+
+  on(eventName, callback) {
+    this.events[eventName] = callback;
+
+    return this;
+  };
+
+  trigger(eventName, data) {
+    if (this.events[eventName]) this.events[eventName].apply(this, data);
+
+    return this;
+  };
+
+  find(key, value) {
+    return this.connections.filter((connection) => {
+      return connection.hasAlias(key, value);
+    })[0];
+  };
+
+  remove(socket) {
+    let socketIndex = this.connections.indexOf(socket);
+
+    if (socketIndex !== -1) this.connections.splice(socketIndex, 1);
+
+    return this;
+  };
+
+};
+
+Socket.Connection = class {
+
+  constructor(socket) {
+    this.connection = socket;
+
+    this.events = {};
+    this.aliases = {};
+
+    return this;
+  };
+
+  on(eventName, callback) {
+    this.events[eventName] = callback;
+    return this;
+  };
+
+  trigger(eventName, data) {
+    if (this.events[eventName]) this.events[eventName].apply(this, data);
+
+    return this;
+  };
+
+  addAlias(key, value) {
+    this.aliases[key] = value;
+
+    return this;
+  };
+
+  hasAlias(key, value) {
+    return (this.aliases[key] != null && this.aliases[key] === value);
+  };
+
+  send(data) {
+    this.connection.send(data);
+
+    return this;
+  };
+
+  isConnected() {
+    return this.connection.connected === true && this.connection.disconnected === false;
+  };
+
+};
+
+module.exports = Socket;
